b525105962e11515f7f87f34e25f3db1d360aa03
joshhovis <joshuahovis3@gmail.com>
2024-07-24 00:15:00 -0400

    Stylesheet paths update for deployment

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ mongoose
 app.locals.moment = require("moment");
 app.set("views", __dirname + "/views");
 app.set("view engine", "ejs");
-app.use(express.static(__dirname + "public"));
+app.use(express.static(__dirname + "/public"));
 
 /****************************/
 /*        Middleware        */
